1929f89e0a37f7a8ea39a7db37fffb8d323e9738
Syntx-09 <116119936+Syntx-09@users.noreply.github.com>
2023-05-06 01:41:15 +0100

    Update dbconn.php

--- inc/dbconn.php
+++ inc/dbconn.php
@@ -9,7 +9,7 @@
    
     try { $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pwd);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "DB connect OK";
+        //echo "DB connect OK";
     } catch (PDOException $e) {
         echo 'Connection failed: <br>' . $e->getMessage();
         }
