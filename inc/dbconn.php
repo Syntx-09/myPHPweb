3a6cf1091c560433a0be53ebce8726f9b0d5af67
SyntX <syntax46am@gmail.com>
2023-05-06 02:05:39 +0100

    update

--- inc/dbconn.php
+++ inc/dbconn.php
@@ -1,15 +1,15 @@
 <?php
 
     $host = 'localhost';
-    $user = 'root';
-    $pwd = '';
-    $dbname = 'personal';
+    $user = 'paradox';
+    $pwd = 'paradox';
+    $dbname = 'myPHPweb';
 
     
    
     try { $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pwd);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "DB connect OK";
+        // echo "DB connect OK";
     } catch (PDOException $e) {
         echo 'Connection failed: <br>' . $e->getMessage();
         }
